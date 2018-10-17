dfd3c29197cc3c367cb190d422a0cfe4956c4f01
kavita <kavita.gupta@hestabit.in>
2018-10-17 10:46:41 +0530

    commit changes

--- packages/chat/message/src/ChatController.php
+++ packages/chat/message/src/ChatController.php
@@ -15,23 +15,27 @@ class ChatController extends Controller
     
     public function get()
     {
-        // get all users except the authenticated one
-        $contacts = User::where('id', '!=', auth()->id())->get();
-        // get a collection of items where sender_id is the user who sent us a message
-        // and messages_count is the number of unread messages we have from him
-        $unreadIds = Message::select(\DB::raw('`from` as sender_id, count(`from`) as messages_count'))
-            ->where('to', auth()->id())
-            ->where('read', false)
-            ->groupBy('from')
-            ->get();
+        try {
+            // get all users except the authenticated one
+            $contacts = User::where('id', '!=', auth()->id())->get();
+            // get a collection of items where sender_id is the user who sent us a message
+            // and messages_count is the number of unread messages we have from him
+            $unreadIds = Message::select(\DB::raw('`from` as sender_id, count(`from`) as messages_count'))
+                ->where('to', auth()->id())
+                ->where('read', false)
+                ->groupBy('from')
+                ->get();
 
-        // add an unread key to each contact with the count of unread messages
-        $contacts = $contacts->map(function($contact) use ($unreadIds) {
-            $contactUnread = $unreadIds->where('sender_id', $contact->id)->first();
-            $contact->unread = $contactUnread ? $contactUnread->messages_count : 0;
-            return $contact;
-        });
-        return response()->json($contacts);
+            // add an unread key to each contact with the count of unread messages
+            $contacts = $contacts->map(function($contact) use ($unreadIds) {
+                $contactUnread = $unreadIds->where('sender_id', $contact->id)->first();
+                $contact->unread = $contactUnread ? $contactUnread->messages_count : 0;
+                return $contact;
+            });
+            return response()->json($contacts);   
+        } catch (Exception $e) {
+            
+        }
     }
 
     public function getMessagesFor($id)
@@ -54,14 +58,22 @@ class ChatController extends Controller
 
     public function send(Request $request)
     {
-        $message = Message::create([
-            'from' => auth()->id(),
-            'to' => $request->contact_id,
-            'text' => $request->text
-        ]);
+        try {
+            if($request->all()){
+                $message = Message::create([
+                    'from' => auth()->id(),
+                    'to' => $request->contact_id,
+                    'text' => $request->text
+                ]);
 
-        broadcast(new NewMessage($message));
-        return response()->json($message);
+                broadcast(new NewMessage($message));
+                return response()->json($message);
+            }else{
+                return response()->json('message','No Data Found');
+            }
+        } catch (Exception $e) {
+            dd($e);
+        }
     }
 }
 
